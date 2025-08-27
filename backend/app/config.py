1a2e4dc01d8ef0229794212ecb9ffad2dbd4b50f
rory <chengithub77@gmail.com>
2025-08-27 11:28:17 +0800

    feat. auto login

--- backend/app/config.py
+++ backend/app/config.py
@@ -9,7 +9,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
-    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(hours=1)  # Access Token 有效期
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=7)  # Access Token 有效期
     JWT_REFRESH_TOKEN_EXPIRES = datetime.timedelta(days=30)  # Refresh Token 有效期
 
     # WTF_CSRF_ENABLED = False
